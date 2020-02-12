c98ac1e8e4aa0b4fe9aa48510798d4bd9b6208ed
Slippar <60616348+Slippar@users.noreply.github.com>
2020-02-12 17:54:45 +0200

    Add files via upload

--- forum.sql
+++ forum.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Feb 06, 2020 at 04:20 PM
+-- Generation Time: Feb 12, 2020 at 04:54 PM
 -- Server version: 10.4.11-MariaDB
 -- PHP Version: 7.4.1
 
@@ -25,16 +25,56 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Table structure for table `categories`
+--
+
+CREATE TABLE `categories` (
+  `idCategories` int(11) NOT NULL,
+  `nameCategories` varchar(50) NOT NULL,
+  `descCategories` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `posts`
+--
+
+CREATE TABLE `posts` (
+  `idPost` int(11) NOT NULL,
+  `contentPost` text NOT NULL,
+  `datePost` datetime NOT NULL,
+  `topicPost` int(11) NOT NULL,
+  `byPost` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `topics`
+--
+
+CREATE TABLE `topics` (
+  `IdTopics` int(11) NOT NULL,
+  `subjectTopics` varchar(255) NOT NULL,
+  `dateTopics` datetime NOT NULL,
+  `catTopics` int(11) NOT NULL,
+  `byTopics` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
 -- Table structure for table `users`
 --
 
 CREATE TABLE `users` (
   `idUsers` int(11) NOT NULL,
-  `uidUsers` varchar(30) DEFAULT NULL,
-  `pwdUsers` varchar(500) DEFAULT NULL,
-  `emailUsers` varchar(50) DEFAULT NULL,
-  `roleUsers` varchar(30) DEFAULT NULL,
-  `regdateUsers` datetime DEFAULT NULL
+  `uidUsers` varchar(30) NOT NULL,
+  `pwdUsers` varchar(500) NOT NULL,
+  `emailUsers` varchar(50) NOT NULL,
+  `roleUsers` varchar(30) NOT NULL DEFAULT '1',
+  `regdateUsers` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
@@ -42,17 +82,74 @@ CREATE TABLE `users` (
 --
 
 INSERT INTO `users` (`idUsers`, `uidUsers`, `pwdUsers`, `emailUsers`, `roleUsers`, `regdateUsers`) VALUES
-(0, 'Dude', '$2y$10$DG.up3xjUKevzynwB67mzesd4lBZaXd5dKPVWOl9YV.UR303mN6P2', 'georgewillbertt@gmail.com', NULL, NULL);
+(0, 'Slippar', '$2y$10$zcwFG8aM/7Q3B8JiT/gHROCVNNWgBDGz2SsOpUtUbFJgGwZM7hZd2', 'georgewillbertt@gmail.com', '1', '2020-02-12 17:31:07');
 
 --
 -- Indexes for dumped tables
 --
 
 --
+-- Indexes for table `categories`
+--
+ALTER TABLE `categories`
+  ADD PRIMARY KEY (`idCategories`),
+  ADD UNIQUE KEY `nameCategories` (`nameCategories`);
+
+--
+-- Indexes for table `posts`
+--
+ALTER TABLE `posts`
+  ADD PRIMARY KEY (`idPost`),
+  ADD KEY `topicPost` (`topicPost`),
+  ADD KEY `byPost` (`byPost`);
+
+--
+-- Indexes for table `topics`
+--
+ALTER TABLE `topics`
+  ADD PRIMARY KEY (`IdTopics`),
+  ADD KEY `catTopics` (`catTopics`),
+  ADD KEY `byTopics` (`byTopics`);
+
+--
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
   ADD PRIMARY KEY (`idUsers`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `posts`
+--
+ALTER TABLE `posts`
+  MODIFY `idPost` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `topics`
+--
+ALTER TABLE `topics`
+  MODIFY `IdTopics` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `posts`
+--
+ALTER TABLE `posts`
+  ADD CONSTRAINT `posts_ibfk_1` FOREIGN KEY (`topicPost`) REFERENCES `topics` (`IdTopics`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `posts_ibfk_2` FOREIGN KEY (`byPost`) REFERENCES `users` (`idUsers`) ON UPDATE CASCADE;
+
+--
+-- Constraints for table `topics`
+--
+ALTER TABLE `topics`
+  ADD CONSTRAINT `topics_ibfk_1` FOREIGN KEY (`catTopics`) REFERENCES `categories` (`idCategories`) ON DELETE CASCADE ON UPDATE CASCADE,
+  ADD CONSTRAINT `topics_ibfk_2` FOREIGN KEY (`byTopics`) REFERENCES `users` (`idUsers`) ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
